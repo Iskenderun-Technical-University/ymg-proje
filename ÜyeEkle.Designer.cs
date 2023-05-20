1cd2be0268a7c61791118208a16c23f7e3dd25c6
karmetta <101903530+karmetta@users.noreply.github.com>
2023-05-20 14:31:28 +0300

    Add files via upload

---
+++ ÜyeEkle.Designer.cs
@@ -0,0 +1,291 @@
+﻿
+namespace WindowsFormsApp5
+{
+    partial class ÜyeEkle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.AdSoyadTb = new System.Windows.Forms.TextBox();
+            this.TelefonNumarasiTb = new System.Windows.Forms.TextBox();
+            this.YasTb = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.AylikTutarTb = new System.Windows.Forms.TextBox();
+            this.CinsiyetCb = new System.Windows.Forms.ComboBox();
+            this.ZamanlamaCb = new System.Windows.Forms.ComboBox();
+            this.btn_ekle = new System.Windows.Forms.Button();
+            this.btn_reset = new System.Windows.Forms.Button();
+            this.btn_geri = new System.Windows.Forms.Button();
+            this.btn_geri2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.DarkRed;
+            this.label1.Location = new System.Drawing.Point(257, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(236, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "FITNESS CENTER";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(283, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(184, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Yeni Üye Ekle ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(52, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Üye Adı Soyadı";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(284, 142);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(141, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Telefon Numarası";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(548, 142);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Yas";
+            // 
+            // AdSoyadTb
+            // 
+            this.AdSoyadTb.Location = new System.Drawing.Point(44, 174);
+            this.AdSoyadTb.Name = "AdSoyadTb";
+            this.AdSoyadTb.Size = new System.Drawing.Size(140, 22);
+            this.AdSoyadTb.TabIndex = 6;
+            // 
+            // TelefonNumarasiTb
+            // 
+            this.TelefonNumarasiTb.Location = new System.Drawing.Point(288, 174);
+            this.TelefonNumarasiTb.Name = "TelefonNumarasiTb";
+            this.TelefonNumarasiTb.Size = new System.Drawing.Size(140, 22);
+            this.TelefonNumarasiTb.TabIndex = 7;
+            // 
+            // YasTb
+            // 
+            this.YasTb.Location = new System.Drawing.Point(502, 174);
+            this.YasTb.Name = "YasTb";
+            this.YasTb.Size = new System.Drawing.Size(140, 22);
+            this.YasTb.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(52, 249);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 20);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Cinsiyet";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(294, 249);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(88, 20);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Aylık Tutar";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(511, 249);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(95, 20);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Zamanlama";
+            // 
+            // AylikTutarTb
+            // 
+            this.AylikTutarTb.Location = new System.Drawing.Point(285, 283);
+            this.AylikTutarTb.Name = "AylikTutarTb";
+            this.AylikTutarTb.Size = new System.Drawing.Size(140, 22);
+            this.AylikTutarTb.TabIndex = 13;
+            // 
+            // CinsiyetCb
+            // 
+            this.CinsiyetCb.FormattingEnabled = true;
+            this.CinsiyetCb.Items.AddRange(new object[] {
+            "Bay",
+            "Bayan"});
+            this.CinsiyetCb.Location = new System.Drawing.Point(44, 281);
+            this.CinsiyetCb.Name = "CinsiyetCb";
+            this.CinsiyetCb.Size = new System.Drawing.Size(151, 24);
+            this.CinsiyetCb.TabIndex = 14;
+            // 
+            // ZamanlamaCb
+            // 
+            this.ZamanlamaCb.FormattingEnabled = true;
+            this.ZamanlamaCb.Items.AddRange(new object[] {
+            "06:00-08:00",
+            "08:00-10:00",
+            "12:00-14:00",
+            "16:00-18:00",
+            "20:00-22:00"});
+            this.ZamanlamaCb.Location = new System.Drawing.Point(502, 281);
+            this.ZamanlamaCb.Name = "ZamanlamaCb";
+            this.ZamanlamaCb.Size = new System.Drawing.Size(151, 24);
+            this.ZamanlamaCb.TabIndex = 15;
+            // 
+            // btn_ekle
+            // 
+            this.btn_ekle.BackColor = System.Drawing.Color.Red;
+            this.btn_ekle.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_ekle.ForeColor = System.Drawing.SystemColors.Window;
+            this.btn_ekle.Location = new System.Drawing.Point(168, 370);
+            this.btn_ekle.Name = "btn_ekle";
+            this.btn_ekle.Size = new System.Drawing.Size(134, 57);
+            this.btn_ekle.TabIndex = 16;
+            this.btn_ekle.Text = "EKLE";
+            this.btn_ekle.UseVisualStyleBackColor = false;
+            this.btn_ekle.Click += new System.EventHandler(this.btn_ekle_Click);
+            // 
+            // btn_reset
+            // 
+            this.btn_reset.BackColor = System.Drawing.Color.Red;
+            this.btn_reset.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_reset.ForeColor = System.Drawing.SystemColors.Window;
+            this.btn_reset.Location = new System.Drawing.Point(333, 370);
+            this.btn_reset.Name = "btn_reset";
+            this.btn_reset.Size = new System.Drawing.Size(134, 57);
+            this.btn_reset.TabIndex = 17;
+            this.btn_reset.Text = "RESET";
+            this.btn_reset.UseVisualStyleBackColor = false;
+            this.btn_reset.Click += new System.EventHandler(this.btn_reset_Click);
+            // 
+            // btn_geri
+            // 
+            this.btn_geri.BackColor = System.Drawing.Color.Red;
+            this.btn_geri.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_geri.ForeColor = System.Drawing.SystemColors.Window;
+            this.btn_geri.Location = new System.Drawing.Point(502, 370);
+            this.btn_geri.Name = "btn_geri";
+            this.btn_geri.Size = new System.Drawing.Size(134, 57);
+            this.btn_geri.TabIndex = 18;
+            this.btn_geri.Text = "GERİ";
+            this.btn_geri.UseVisualStyleBackColor = false;
+            this.btn_geri.Click += new System.EventHandler(this.btn_geri_Click);
+            // 
+            // btn_geri2
+            // 
+            this.btn_geri2.BackColor = System.Drawing.Color.Red;
+            this.btn_geri2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_geri2.ForeColor = System.Drawing.SystemColors.Window;
+            this.btn_geri2.Location = new System.Drawing.Point(333, 457);
+            this.btn_geri2.Name = "btn_geri2";
+            this.btn_geri2.Size = new System.Drawing.Size(134, 57);
+            this.btn_geri2.TabIndex = 19;
+            this.btn_geri2.Text = "GERİ";
+            this.btn_geri2.UseVisualStyleBackColor = false;
+            this.btn_geri2.Click += new System.EventHandler(this.btn_geri2_Click);
+            // 
+            // ÜyeEkle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(903, 566);
+            this.Controls.Add(this.btn_geri2);
+            this.Controls.Add(this.btn_geri);
+            this.Controls.Add(this.btn_reset);
+            this.Controls.Add(this.btn_ekle);
+            this.Controls.Add(this.ZamanlamaCb);
+            this.Controls.Add(this.CinsiyetCb);
+            this.Controls.Add(this.AylikTutarTb);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.YasTb);
+            this.Controls.Add(this.TelefonNumarasiTb);
+            this.Controls.Add(this.AdSoyadTb);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "ÜyeEkle";
+            this.Text = "ÜyeEkle";
+            this.Load += new System.EventHandler(this.ÜyeEkle_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox AdSoyadTb;
+        private System.Windows.Forms.TextBox TelefonNumarasiTb;
+        private System.Windows.Forms.TextBox YasTb;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox AylikTutarTb;
+        private System.Windows.Forms.ComboBox CinsiyetCb;
+        private System.Windows.Forms.ComboBox ZamanlamaCb;
+        private System.Windows.Forms.Button btn_ekle;
+        private System.Windows.Forms.Button btn_reset;
+        private System.Windows.Forms.Button btn_geri;
+        private System.Windows.Forms.Button btn_geri2;
+    }
+}
